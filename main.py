7238cae3419756d7f0d776642de2560b942baba7
sajad <sajad.hardani188@gmail.com>
2025-12-20 23:18:17 +0330

    Change: library to (telebot) add: python-dotenv

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
 
-import asyncio
+import telebot
+from telebot import types
+from dotenv import dotenv_values
 
-from aiogram import Dispatcher, filters
-from aiogram.types import Message
-from aiogram import Bot
+config = dotenv_values(".env")
+TOKEN = config["TOKEN"]
 
+bot = telebot.TeleBot(TOKEN)
 
-dp = Dispatcher()
+# دستور /start
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(
+        message,
+        "به ربات دانلود فیلم خوش اومدی 🎬\n\n"
+        "برای دانلود فیلم عضو بخش membership شو 👇\n"
+        "/membership"
+    )
 
-
-@dp.message(filters.CommandStart())
-async def start(message: Message):
-    print(message)
-
-
-
-async def main():
-    bot = Bot(token="8208114695:AAGc4uZwaIfgvHWNz1pFVPbZ_AnSv9WO0Kc")
-    await dp.start_polling(bot)
-
+# دستور /membership
+@bot.message_handler(commands=['membership'])
+def membership(message):
+    bot.reply_to(
+        message,
+        "💳 برای خرید اشتراک با ادمین تماس بگیر:\n@sajadmyy"
+    )
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    bot.infinity_polling()
